d4278e5d5ef70f31a3a48dce10cf7c988b4f7094
Mishthi Garg <mishthigarg1306@gmail.com>
2026-07-04 03:46:37 +0530

    initial ml service

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("grade_predictor_model.pkl")
+
+class PredictionInput(BaseModel):
+    overall_z: float
+
+@app.post("/predict")
+def predict_grade(input: PredictionInput):
+    features = np.array([[input.overall_z]])
+    predicted_grade = model.predict(features)[0]
+
+    probabilities = model.predict_proba(features)[0]
+    grade_classes = model.classes_
+    confidence_scores = {grade: round(float(prob),3) for grade,prob in zip(grade_classes, probabilities)}
+
+    return{
+        "predicted_grade": predicted_grade,
+        "confidence": confidence_scores
+    }
+@app.get("/")
+def health_check():
+    return{
+        "status": "LearnTrack ML service running"
+    }
